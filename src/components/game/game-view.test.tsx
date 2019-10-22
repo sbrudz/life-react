6213ee34a267e389db6558f1d7d5ee809f2a2bc6
Steve Brudz <sbrudz@defmethod.io>
2019-10-22 16:21:49 -0500

    Draw grid based on number of cells set in the input field

--- src/components/game/game-view.test.tsx
+++ src/components/game/game-view.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Game from ".";
 
 describe("<Game />", () => {
@@ -16,5 +16,46 @@ describe("<Game />", () => {
   it("renders a Step button", () => {
     const { getByText } = render(<Game />);
     expect(getByText("Step")).toBeInTheDocument();
+  });
+
+  it("renders a Size label and input", () => {
+    const { getByLabelText } = render(<Game />);
+    expect(getByLabelText("Size")).toBeInTheDocument();
+  });
+
+  it("renders a Size input that defaults to 20", () => {
+    const { getByLabelText } = render(<Game />);
+    const sizeInput = getByLabelText("Size");
+    expect(sizeInput).toHaveValue(20);
+  });
+
+  it("renders a grid that has 20 rows", () => {
+    const { getByTitle } = render(<Game />);
+    const grid = getByTitle("Grid");
+    expect(grid.getElementsByTagName("tr").length).toEqual(20);
+  });
+
+  it("renders a grid that has 20 columns", () => {
+    const { getByTitle } = render(<Game />);
+    const grid = getByTitle("Grid");
+    const rows = grid.getElementsByTagName("tr");
+    for (let i = 0; i < rows.length; i++) {
+      expect(rows[i].getElementsByTagName("td").length).toEqual(20);
+    }
+  });
+
+  it("resizes the grid when Size is changed", () => {
+    const newSize = 25;
+    const { getByTitle, getByLabelText } = render(<Game />);
+
+    const sizeInput = getByLabelText("Size");
+    fireEvent.change(sizeInput, { target: { value: newSize } });
+
+    const grid = getByTitle("Grid");
+    const rows = grid.getElementsByTagName("tr");
+    expect(rows.length).toEqual(newSize);
+    for (let i = 0; i < rows.length; i++) {
+      expect(rows[i].getElementsByTagName("td").length).toEqual(newSize);
+    }
   });
 });
--- src/components/game/game-view.tsx
+++ src/components/game/game-view.tsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import "./game.css";
 
 const Game: React.FC = () => {
+  const [size, setSize] = useState(20);
+  const grid = useMemo(() => {
+    const rows = new Array(size);
+    const defaultRow: boolean[] = new Array(size);
+    defaultRow.fill(false);
+    rows.fill(defaultRow);
+    return rows;
+  }, [size]);
+
   return (
-    <section>
+    <section className="Game-section">
       <h2>Game</h2>
-      <table title="Grid"></table>
+      <table title="Grid">
+        <tbody>{buildCells(grid)}</tbody>
+      </table>
+      <label htmlFor="size">Size</label>
+      <input
+        type="number"
+        id="size"
+        value={size}
+        onChange={e => setSize(+e.target.value)}
+      />
       <button>Step</button>
     </section>
   );
+};
+
+const buildCells = (grid: boolean[][]) => {
+  return grid.map((row, rowIdx) => {
+    const children = row.map((cellVal, colIdx) => (
+      <td key={`cell-${rowIdx}-${colIdx}`}>{cellVal ? 1 : 0}</td>
+    ));
+    return <tr key={`row-${rowIdx}`}>{children}</tr>;
+  });
 };
 
 export default Game;
